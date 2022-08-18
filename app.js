4fd2620e404c4755c1183421abc169cc652ba9b7
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-08-18 21:49:52 +0530

    add

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const https = require("https");
 
 const app = express();
-
 app.get("/",function(req,res){
     const url ="https://api.openweathermap.org/data/2.5/weather?q=London&appid=3057a14e23347af71adb0492ecd5c080&units=metrix";
     https.get(url, function(response){
